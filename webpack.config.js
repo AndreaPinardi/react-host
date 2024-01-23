d9e8b6be6649ab9ba394e7df1ae45e85ddb30057
Andrea Pinardi <a.pinardi@reply.it>
2024-01-23 10:44:32 +0100

    fix public path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "https://react-host-nine.vercel.app",
+    publicPath: argv.mode === "development" ? "http://localhost:8080" : "https://react-host-nine.vercel.app/",
   },
 
   resolve: {
