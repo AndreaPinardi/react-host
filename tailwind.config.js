ab8dfc95c8154ca5f2da3343bca89652c6b2c4b2
Andrea Pinardi <a.pinardi@reply.it>
2024-01-26 12:38:47 +0100

    fix tailwind theming and css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--base---primary)',
+        secondary: 'var(--light---primary)',
+      },
+      fontFamily: {
+        primary: 'var(--font--primary)'
+      }
+    },
   },
   variants: {
     extend: {},
